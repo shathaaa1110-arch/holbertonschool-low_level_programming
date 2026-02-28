1f960ac9c1bb79b0536e6197f57ac203d794aca3
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-02-28 19:42:52 +0000

    task

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+	char letter;
+
+	count = 0;
+	while (count < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
