6ad344e1062c8f0c2ff40221699bf1dcf245adac
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-02-28 14:40:43 +0000

    task1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h> /* مكتبة الطباعة حقتنا */
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit; /* سوينا صندوق جديد عشان نحفظ فيه الخانة الأخيرة */
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* هنا السحر: سحبنا الخانة الأخيرة وحطيناها في صندوقنا الجديد */
+	last_digit = n % 10;
+
+	/* الحين نختبر الصندوق الجديد حقنا */
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	}
+
+	return (0);
+}
