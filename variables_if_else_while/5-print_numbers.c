ad13496dfa5c3c89da435cd87c21e1511f25f99d
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-02-28 18:33:32 +0000

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
