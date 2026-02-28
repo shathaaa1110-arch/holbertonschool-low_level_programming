be3c8cbad31c5fd372ce346af421c4f1b3669b8e
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-02-28 19:36:06 +0000

    task

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
