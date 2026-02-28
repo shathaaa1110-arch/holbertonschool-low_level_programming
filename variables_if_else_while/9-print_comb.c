bc11e041c9cd64c8d1042687f799c5880b9f8f45
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-02-28 18:47:47 +0000

    task9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		putchar(n + '0');
+
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
