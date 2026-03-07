c04586131ba42557677a1e86ed3ed7760d14d3ac
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-03-07 13:48:40 +0000

    t0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	/* نشيك إذا الحرف يقع بين A و Z */
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1); /* إذا صح، رجع 1 */
+	}
+	
+	return (0); /* غير كذا، رجع 0 */
+}
