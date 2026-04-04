7a8c63559a1cda23e46e389d3a8d6c2c8a42ca34
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-04-04 08:47:48 +0000

    0. Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: structure containing dog basic details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
